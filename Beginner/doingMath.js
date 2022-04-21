442a3c719017bee943e0aa441ddce1991e33e304
Jarrett Parker <jparker@isystemsweb.com>
2022-04-21 09:16:58 -0400

    Finished doing math page

---
+++ Beginner/doingMath.js
@@ -0,0 +1,8 @@
+var apples = 5;
+var pears = 10;
+
+var piecesOfFruit = apples + pears;
+
+document.write("apples = " + apples + "\n");
+document.write("pears = " + pears + "\n");
+document.write("Pieces of Fruit = " + piecesOfFruit + "\n");
--- Beginner/variables.js
+++ Beginner/variables.js
@@ -1,1 +1,2 @@
 var name = prompt('Please Type in Your Name');
+document.write(name);
